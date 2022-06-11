4c098a7ae09d413e9a48c964986c4bff8cc74481
Johnnie Ho <johnnieho89@gmail.com>
2022-06-11 21:12:21 +0200

    feat: output the pushed chart version

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,8 @@ set -e
 helm registry login $1 --username $2 --password $3
 helm package $4
 FILE_NAME=$(ls | grep ecommerce-)
+VERSION=${FILE_NAME#*-}
+VERSION=${VERSION%.*}
+
 helm push $FILE_NAME oci://$1/$5
+echo ::set-output name=chartVersion::"$VERSION"
